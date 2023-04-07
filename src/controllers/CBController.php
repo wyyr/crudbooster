f6b06377a044d2d832b0dfdb0c43833d69871cdf
Waya PR <wayaragil66@gmail.com>
2023-04-07 15:02:04 +0700

    refactor: add dialog swal method

--- src/controllers/CBController.php
+++ src/controllers/CBController.php
@@ -1773,6 +1773,23 @@ class CBController extends Controller
         }
     }
 
+    public function DialogConfirm($url, $message = null)
+    {
+        return "Swal.fire({
+            title: 'Are you sure ?',
+            text: '$message',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = '$url';
+            }
+        })";
+    }
+
     public function actionButtonSelected($id_selected, $button_name)
     {
     }
